2514296bbc712204dce307cf084437e61e266352
Checkout <Yusuf-Juzar-Soni>
2021-06-12 12:50:19 -0700

    Cleaned Code

--- Backend/config/keys.js
+++ Backend/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://yusuf:test1234@spitwisev2.xhg1h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  secretOrKey: "secret",
+    "<aad your own db URI>",
+  secretOrKey: "<add your own key>",
 };
